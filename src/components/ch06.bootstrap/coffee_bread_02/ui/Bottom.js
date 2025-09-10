1fe8fd0eede25c8e8d46d546ada4054c742e6ef6
lcg54 <rjscodla98@naver.com>
2025-09-10 18:16:18 +0900

    bootstrap

--- src/components/ch06.bootstrap/coffee_bread_02/ui/Bottom.js
+++ src/components/ch06.bootstrap/coffee_bread_02/ui/Bottom.js
@@ -1,7 +1,7 @@
 import { ButtonGroup, Button } from "react-bootstrap";
 
 function App() {
-  const message = '카운터에서 주문해 주시고, 즐거운 시간 되시길 바랍니다.';
+  const message = '카운터에서 주문해 주세요. 즐거운 시간 되시길 바랍니다.';
 
   const ClickMenu = () => {
 
@@ -15,7 +15,7 @@ function App() {
         <Button variant="secondary" onClick={ClickMenu}>삭제</Button>
         <Button variant="secondary" onClick={ClickMenu}>카테고리 추가</Button>
       </ButtonGroup>
-      <br />
+      <br /><br />
       {message}
     </>
   );
--- src/components/ch06.bootstrap/coffee_bread_02/ui/Display.js
+++ src/components/ch06.bootstrap/coffee_bread_02/ui/Display.js
@@ -1,34 +1,42 @@
-import { Table, Image } from "react-bootstrap";
+import { useState } from "react";
+
+import { Table, Image, CloseButton } from "react-bootstrap";
 import './../css/Display.css';
 
 // 모드에 따라서 화면을 분기해주는 스위치 앱
 function App({ product }) {
-
-  const DetailTable = () => {
-    return (
-      <Table striped bordered hover>
-        <tbody>
-          <tr><td className="detailLeft">상품번호</td><td>{product.id}</td></tr>
-          <tr><td className="detailLeft">이름</td><td>{product.name}</td></tr>
-          <tr><td className="detailLeft">단가</td><td>{Number(product.price).toLocaleString()} 원</td></tr>
-          <tr><td className="detailLeft">카테고리</td><td>{product.category === 'bread' ? '빵' : '음료수'}</td></tr>
-          <tr><td className="detailLeft">재고</td><td>{product.stock}</td></tr>
-        </tbody>
-      </Table>
-    );
-  }
   
+  const [showDetailedTable, setshowDetailedTable] = useState(true);
+  const handleClose = () => setshowDetailedTable(boolean => !boolean);
+
   return (
     <>
-      <Table bordered hover>
-        <tbody>
-          <tr>
-            <td className="detailLeft"><DetailTable /></td>
-            <td><Image src={`/images/${product.image}`} alt={product.name} rounded /></td>
-            <td>{product.description}</td>
-          </tr>
-        </tbody>
-      </Table>
+      <CloseButton onClick={handleClose} />
+        {showDetailedTable && (
+          <Table bordered hover>
+            <tbody>
+              <tr>
+                <td className="leftOfTable">
+                  <Table striped bordered hover>
+                    <tbody>
+                      <tr><td className="leftOfTable">상품번호</td><td>{product.id}</td></tr>
+                      <tr><td className="leftOfTable">이름</td><td>{product.name}</td></tr>
+                      <tr><td className="leftOfTable">단가</td><td>{Number(product.price).toLocaleString()} 원</td></tr>
+                      <tr><td className="leftOfTable">카테고리</td><td>{product.category === 'bread' ? '빵' : '음료수'}</td></tr>
+                      <tr><td className="leftOfTable">재고</td><td>{product.stock}</td></tr>
+                    </tbody>
+                  </Table>
+                </td>
+                <td>
+                  <Image src={`/images/${product.image}`} alt={product.name} rounded />
+                </td>
+                <td className="rightOfTable">
+                  {product.description}
+                </td>
+              </tr>
+            </tbody>
+          </Table>
+        )}
     </>
   );
 }
